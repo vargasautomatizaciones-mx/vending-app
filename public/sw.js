29674d02cd35a4879ef09497930ae08fff82bf98
Ruben Vargas <ruben.vargas@qts.local>
2026-02-24 12:55:52 -0600

    Fix zombie service worker (Network First)

--- public/sw.js
+++ public/sw.js
@@ -15,8 +15,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
